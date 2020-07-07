ea6d92c31e090336d94cbe1422ce5afca23b7ce0
ZQWhiting <zach.whiting@icloud.com>
2020-07-07 14:38:44 -0600

    added psuedocode

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,22 +4,39 @@ const { createNote, deleteNote } = require('../../lib/notes')
 let { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
+    // get the notes from the database
     let results = notes;
+
+    // return the notes from the database
     res.json(results);
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = shortid.generate();
+    // get the note to be created
+    let note = req.body;
 
-    const note = createNote(req.body, notes)
+    // generate a unique id for the note
+    note.id = shortid.generate();
 
+    // push the note into the database
+    createNote(note, notes)
+
+    // return the created note
     res.json(note);
 });
 
 router.delete('/notes/:id', (req, res) => {
-    updArr = deleteNote(req.params.id, notes);
+    // get the id of the note to be deleted
+    const id = req.params.id;
+
+    // delete the note from the database
+    updArr = deleteNote(id, notes);
+
+    // set the value of notes to the updated array
     notes = updArr;
-    res.json(notes);
+
+    // return the updated array
+    res.json(updArr);
 });
 
 module.exports = router;
